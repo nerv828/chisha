c1a6c7987f42bd3ae9db852a040362b348d10e0d
root <nerv828@gmail.com>
2012-09-12 00:38:55 +0800

    add new models and tests

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -5,9 +5,36 @@ import models.*;
 
 public class BasicTest extends UnitTest {
 
+	@Before
+	public void setup(){
+			Fixtures.deleteDatabase();
+	}
     @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    public void createAndRetrieveUser() {
+    		new User("bob@gmail.com","secret","Bob").save();
+    		User bob=User.find("byEmail","bob@gmail.com").first();
+    		assertNotNull(bob);
+    		assertEquals("Bob",bob.fullname);
     }
-
-}
+    @Test	
+    public  void tryConnectAsUser(){
+     		new  User("bob@gmail.com","secret","Bob").save();
+     		assertNotNull(User.connect("bob@gmail.com","secret"));
+     		assertNull(User.connect("bob@gmail.com","badsecret"));
+     		assertNull(User.connect("toy@gmail.com","secret"));
+    }
+	@Test
+	public void createPost(){
+			User bob=new User("bob@gmail.com","secret","Bob").save();
+			new Post(bob,"My first Post","Hello world!!!").save();
+			assertEquals(1,Post.count());
+			List<Post> bobPost=Post.find("byAuthor",bob).fetch();
+			assertEquals(1,bobPost.size());
+			Post firstPost=bobPost.get(0);
+			assertNotNull(firstPost);
+			assertEquals(bob,firstPost.author);
+			assertEquals("My first Post",firstPost.title);
+			assertEquals("Hello world!!!",firstPost.content);
+			assertNotNull(firstPost.postedAt);
+	}
+}	
